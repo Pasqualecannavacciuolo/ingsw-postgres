178162c63be2c11a70a6e87eb4d63df66237e82f
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2023-05-30 19:05:57 +0200

    Modifiche che aggiungono il pull di pgdata da github

--- create.sh
+++ create.sh
@@ -5,11 +5,29 @@ LIGHT_GREEN='\033[1;32m'
 LIGHT_RED='\033[1;31m'
 NC='\033[0m' # No colore
 
+# Identifico il Sistema Operativo
+platform='unknown'
+OS=$(uname)
+if [[ "$OS" == 'Darwin' ]]; then
+   platform='mac'
+elif [[ "$unamestr" != 'Darwin' ]]; then
+   platform='linux'
+fi
+
+# Linea di DEBUG
+#echo $platform
+
 # Installazione requisiti
-sudo apt install jp2a python3
+if [[ "$platform" != 'mac' ]]; then
+    echo "Entri anche qui sfigato"
+    sudo apt install jp2a python3
+elif [[ "$platform" == 'mac' ]]; then
+   echo -e  "✅ ${LIGHT_GREEN} Sei su Mac nulla da fare per ora...${NC}"
+fi
+#sudo apt install jp2a python3
 
 # Aggiungo le librerie python
-pip install gdown docker
+pip install gdown docker --user
 
 # Creo l'immagine di PepeTheFrog
 jp2a Pepe_small.png --colors --width=25 --height=15
@@ -17,11 +35,16 @@ jp2a Pepe_small.png --colors --width=25 --height=15
 echo "📣 Processo di creazione del container 🐳Docker avviato"
 
 echo "⌛️ Ottengo la cartella pgdata..."
-python3 gdrive_pgdata.py
-DIR=./pgdata
+mkdir data_folder
+cd data_folder
+git clone https://github.com/Pasqualecannavacciuolo/pgdata_repo.git
+cd pgdata_repo
+echo $(pwd)
+python3 ../../gdrive_pgdata.py
+DIR=pgdata
 if [ ! -d "$DIR" ];
 then
-	echo -e "🔴 ${LIGHT_RED} Cartella non ottenuta"
+	echo -e "🔴 ${LIGHT_RED} Cartella non ottenuta${NC}"
 else
 	echo -e "✅ ${LIGHT_GREEN}Cartella ottenuta con successo${NC}"
 fi
@@ -29,16 +52,19 @@ fi
 # Setto la proprietà della cartella pgdata sull'attuale utente
 USER=$(whoami)
 sudo chown -R $USER pgdata
+cd .. ..
+
 
 echo "⌛️ Avvio del container 🐳Docker..."
 
 # Creo il container per PostgreSQL
-docker run --name INGSW-postgres-bash -p 5432:5432 -e POSTGRES_PASSWORD=progetto123 -e POSTGRES_DB=ristorante -e POSTGRES_USER=pasquale -e PGDATA=/pgdata -v $(pwd)/pgdata:/pgdata -d postgres
-
+cd ..
+docker run --name INGSW-postgres-bash -p 5432:5432 -e POSTGRES_PASSWORD=progetto123 -e POSTGRES_DB=ristorante -e POSTGRES_USER=pasquale -e PGDATA=/pgdata -v $(pwd)/data_folder/pgdata_repo/pgdata:/pgdata -d postgres
+echo $(pwd)
 containerIsRunning=$(python3 container_is_running.py)
 if [ "$containerIsRunning" = "is running" ];
 then
-	echo -e "✅ ${LIGHT_GREEN}Container creato ed attualmente in esecuzione"
+	echo -e "✅ ${LIGHT_GREEN}Container creato ed attualmente in esecuzione${NC}"
 else
-	echo -e "🔴 ${LIGHT_RED} Container non creato correttamente, rivedi il comando di creazione"
+	echo -e "🔴 ${LIGHT_RED} Container non creato correttamente, rivedi il comando di creazione${NC}"
 fi
